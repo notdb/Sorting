18a783bb6b8d745bec548e60113a398f5d218dcc
Doug <me@douglasbowen.com>
2019-09-09 16:19:40 -0400

    add notes to selection sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,11 +1,26 @@
-# TO-DO: Complete the selection_sort() function below 
+# TO-DO: Complete the selection_sort() function below
+
+"""
+    Separate the first element from the rest of the array. Think about it as a sorted list of one element.
+
+    For all other indices, beginning with [1]:
+
+    a. Copy the item at that index into a temp variable
+
+    b. Iterate to the left until you find the correct index in the "sorted" part of the array at which this element should be inserted
+        Shift items over to the right as you iterate
+
+    c. When the correct index is found, copy temp into this position
+"""
+
 def selection_sort( arr ):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
+    for i in range(0, len(arr) - 1): ## starting at 1, len(arr)-1 starts at 1, because range is 0 or somethign of that nature
+        cur_index = i ## temp variable
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
+        # (hint, can do in 3 loc)
+        
              
 
 
